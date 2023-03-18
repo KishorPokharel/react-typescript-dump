0db15e6083c7c46a955daaf3d59909575a5cda9b
KishorPokharel <kishorpokharel7@gmail.com>
2023-03-18 08:38:19 +0545

    generic http service

--- src/App.tsx
+++ src/App.tsx
@@ -304,7 +304,97 @@ import userService, { User } from "./services/user-service";
 //   );
 // };
 
-// loaders
+// loaders and http
+// const App = () => {
+//   const [users, setUsers] = useState<User[]>([]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     setLoading(true);
+//     const { request, cancel } = userService.getAllUsers();
+//     request
+//       .then((res) => {
+//         setUsers(res.data);
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         if (err instanceof CanceledError) return;
+//         setError(err.message);
+//         setLoading(false);
+//       });
+
+//     return () => cancel();
+//   }, []);
+
+//   const deleteUser = (user: User) => {
+//     const originalUsers = [...users];
+//     setUsers(users.filter((u) => u.id !== user.id));
+//     userService.deleteUser(user.id).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   const addUser = () => {
+//     const originalUsers = [...users];
+//     const newUser = { id: 0, name: "Kishor" };
+//     setUsers([newUser, ...users]);
+
+//     userService
+//       .createUser(newUser)
+//       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+//       .catch((err) => {
+//         setError(err.message);
+//         setUsers(originalUsers);
+//       });
+//   };
+
+//   const updateUser = (user: User) => {
+//     const originalUsers = [...users];
+//     const updatedUser = { ...user, name: user.name + "!" };
+//     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+//     userService.updateUser(updatedUser).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   return (
+//     <>
+//       <p className="text-danger">{error}</p>
+//       {isLoading && <div className="spinner-border"></div>}
+//       <button className="btn btn-primary mb-3" onClick={addUser}>
+//         Add User
+//       </button>
+//       <ul className="list-group">
+//         {users.map((user) => (
+//           <li
+//             className="list-group-item d-flex justify-content-between"
+//             key={user.id}
+//           >
+//             {user.name}{" "}
+//             <div>
+//               <button
+//                 className="btn btn-outline-secondary mx-1"
+//                 onClick={() => updateUser(user)}
+//               >
+//                 Update
+//               </button>
+//               <button
+//                 className="btn btn-outline-danger"
+//                 onClick={() => deleteUser(user)}
+//               >
+//                 Delete
+//               </button>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
@@ -312,7 +402,7 @@ const App = () => {
 
   useEffect(() => {
     setLoading(true);
-    const { request, cancel } = userService.getAllUsers();
+    const { request, cancel } = userService.getAll<User>();
     request
       .then((res) => {
         setUsers(res.data);
@@ -330,7 +420,7 @@ const App = () => {
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
-    userService.deleteUser(user.id).catch((err) => {
+    userService.delete(user.id).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -342,7 +432,7 @@ const App = () => {
     setUsers([newUser, ...users]);
 
     userService
-      .createUser(newUser)
+      .create(newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
@@ -354,7 +444,7 @@ const App = () => {
     const originalUsers = [...users];
     const updatedUser = { ...user, name: user.name + "!" };
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-    userService.updateUser(updatedUser).catch((err) => {
+    userService.update(updatedUser).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
