6c1b49c632b2d33a1dcb0df8589e8d19cb239a4f
KishorPokharel <kishorpokharel7@gmail.com>
2023-03-18 08:44:28 +0545

    custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ProductList from "./components/ProductList";
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 // function App() {
 //   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -395,27 +396,100 @@ import userService, { User } from "./services/user-service";
 //   );
 // };
 
+// http call with generic http service
+// const App = () => {
+//   const [users, setUsers] = useState<User[]>([]);
+//   const [error, setError] = useState("");
+//   const [isLoading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     setLoading(true);
+//     const { request, cancel } = userService.getAll<User>();
+//     request
+//       .then((res) => {
+//         setUsers(res.data);
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         if (err instanceof CanceledError) return;
+//         setError(err.message);
+//         setLoading(false);
+//       });
+
+//     return () => cancel();
+//   }, []);
+
+//   const deleteUser = (user: User) => {
+//     const originalUsers = [...users];
+//     setUsers(users.filter((u) => u.id !== user.id));
+//     userService.delete(user.id).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   const addUser = () => {
+//     const originalUsers = [...users];
+//     const newUser = { id: 0, name: "Kishor" };
+//     setUsers([newUser, ...users]);
+
+//     userService
+//       .create(newUser)
+//       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+//       .catch((err) => {
+//         setError(err.message);
+//         setUsers(originalUsers);
+//       });
+//   };
+
+//   const updateUser = (user: User) => {
+//     const originalUsers = [...users];
+//     const updatedUser = { ...user, name: user.name + "!" };
+//     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+//     userService.update(updatedUser).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   return (
+//     <>
+//       <p className="text-danger">{error}</p>
+//       {isLoading && <div className="spinner-border"></div>}
+//       <button className="btn btn-primary mb-3" onClick={addUser}>
+//         Add User
+//       </button>
+//       <ul className="list-group">
+//         {users.map((user) => (
+//           <li
+//             className="list-group-item d-flex justify-content-between"
+//             key={user.id}
+//           >
+//             {user.name}{" "}
+//             <div>
+//               <button
+//                 className="btn btn-outline-secondary mx-1"
+//                 onClick={() => updateUser(user)}
+//               >
+//                 Update
+//               </button>
+//               <button
+//                 className="btn btn-outline-danger"
+//                 onClick={() => deleteUser(user)}
+//               >
+//                 Delete
+//               </button>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// using custom data fetching hook
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
